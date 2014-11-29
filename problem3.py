8691940d3e7a1537284c4b72b67b91b6f940efd0
allentran <realallentran@gmail.com>
2014-11-28 17:56:41 -0800

    prob 3 sol

--- problem3.py
+++ problem3.py
@@ -1,10 +1,13 @@
-idx = 600851475143-2
-tot = 600851475143
+# Use idea that every integer is product of primes.  Find a small prime factor, and then recursively find the rest.
+
+factorize_num = 600851475143
+
+odd_num = 3
 while True:
-
-    if tot % idx == 0:
-        print idx
+    if factorize_num % odd_num == 0:
+        print factorize_num/odd_num, odd_num
+        factorize_num = factorize_num/odd_num
+        odd_num = 3
+    if odd_num > factorize_num/2:
         break
-    idx -= 2
-
-
+    odd_num += 2
